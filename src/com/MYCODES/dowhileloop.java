9495cd6ca418c43b084da16f7fa9148c8983de2d
amanrashm <amanra664@gmail.com>
2022-04-30 09:06:51 +0530

    Initial commit

---
+++ src/com/MYCODES/dowhileloop.java
@@ -0,0 +1,14 @@
+package com.MYCODES;
+
+public class dowhileloop {
+
+    public static void main(String[] args) {
+	// write your code here
+        int j = 0;
+do {
+    System.out.println(j);
+    j+=1;
+}
+while (j<100);
+    }
+}
